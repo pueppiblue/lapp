8c50d07b00b83dae23fc043ae84d62884cac28ee
Nils Freigang <Nils.Freigang@gmx.de>
2016-04-05 18:15:02 +0200

    remove annotation routes add mock logic to BookTypeTest

--- tests/AppBundle/Form/Type/BookTypeTest.php
+++ tests/AppBundle/Form/Type/BookTypeTest.php
@@ -8,20 +8,47 @@
 
 namespace Tests\AppBundle\Form\Type;
 
-use Symfony\Component\Form\Tests\TestTypeCase;
 use AppBundle\Form\Type\BookType;
+use Symfony\Component\Form\PreloadedExtension;
+use Symfony\Component\Form\Test\TypeTestCase;
 
 
 class BookTypeTest extends TypeTestCase
 {
+    private $em;
+
+    protected function setUp()
+    {
+        // mock dependencies
+        $this->em = $this->getMock('Doctrine\Common\Persistence\ObjectManager');
+
+        parent::setUp();
+    }
+
+    protected function getExtensions()
+    {
+        // create booktype instance with mocked dependencies
+        $bookType = new BookType($this->em);
+
+        return array(
+            new PreloadedExtension(array($bookType), array()),
+        );
+    }
     public function testSubmitValidData()
     {
-        $formData= array(
-            'test1' => 'test1',
-            'test2' => 'test2',
+        $formData = array(
+            'title' => 'Test titel',
+            'author' => 'der unit tester',
+            'isbn' => '123445',
+            'price' => '99',
         );
 
-        $form = $this->factory->create(TestedType::class);
+
+        $form = $this->factory->create(BookType::class);
+
+        //$form->submit($formData);
+
+        $this->assertTrue($form->isSynchronized());
 
     }
 }
