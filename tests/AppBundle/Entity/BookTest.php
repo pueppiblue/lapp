d48152d94626c28d2c2610827990fd524d0633da
pueppiblue <Nils.Freigang@gmx.de>
2016-04-06 21:13:32 +0200

    Add BookTest.php
      add dataprovider BookTests::provideBookData
      add BookTest::testGetBookReturnsExpectedBookOjects

---
+++ tests/AppBundle/Entity/BookTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pueppi
+ * Date: 06.04.16
+ * Time: 19:28
+ */
+
+namespace tests\AppBundle\Entity;
+
+
+class BookTest extends \PHPUnit_Framework_TestCase
+{
+
+}
