c5f68507e0821a5b92513584295a21ff16adb532
pueppiblue <Nils.Freigang@gmx.de>
2016-04-14 17:01:44 +0200

    Compile materialize.scss with gulp
      This loads the gulp-sass compiler with npm
      and creates  new gulp-task to compile the
      materialize.scss to web/css/materialize.css.
      We also add the css-folder to .gitignore because
      all of its content will be managed by gulp tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,11 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('getcss', function(){
+    gulp.src('web/assets/vendor/Materialize/sass/materialize.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('web/css'));
+});
 
 gulp.task('default', function() {
     console.log('Testing Gulp script')
