f7a0c9dfc476cf78fae9ff7d2b7db145d26824cf
pueppiblue <Nils.Freigang@gmx.de>
2016-04-15 15:29:27 +0200

    Add sourcemap package to gulp #29
      This imports gulp-sourcemaps package as dev dependency
      and uses the gulp task to create a sourcemap for
      our css file.

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,15 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('getcss', function(){
     gulp.src('web/assets/vendor/Materialize/sass/materialize.scss')
+        .pipe(sourcemaps.init())
         .pipe(sass())
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('web/css'));
 });
 
 gulp.task('default', function() {
-    console.log('Testing Gulp script')
+console.log('Testing Gulp script')
 });
